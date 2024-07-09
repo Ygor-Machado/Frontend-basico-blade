0a7468e65659a565ae246f532c798a19b538bd51
Ygor-Machado <ygaounderground011@gmail.com>
2024-07-09 01:41:16 -0300

    feat: compilando assets com alpinejs

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -3,21 +3,25 @@
 <head>
     <meta charset="UTF-8">
     {{--    @vite('resources/css/app.css')--}}
-    <title>{{ $title ?? "MLA" }}</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <title>Laravel</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-<div class="min-h-full">
+<body class="min-h-screen w-full flex items-center">
 
-    <x-layouts.navigation></x-layouts.navigation>
-
-    <div class="py-10">
-        <main>
-            {{ $slot }}
-        </main>
+    <div class="flex flex-col space-y-4" x-data="{visible: false}">
+        <div x-show="visible" class="w-[180px] h-[80px] bg-blue-base shadow-hard"></div>
+        <button @click="visible = !visible" class="w-[180px] h-[80px] bg-blue-100 shadow-regular">
+            Vai corinthians
+        </button>
+        <div class="w-[180px] h-[80px] bg-blue-200 shadow-medium"></div>
+        <div class="w-[180px] h-[80px] bg-blue-300 shadow-big"></div>
+        <div class="w-[180px] h-[80px] bg-blue-400 shadow-card"></div>
+        <div class="w-[180px] h-[80px] bg-blue-500 shadow-badge"></div>
     </div>
-
-</div>
 
 </body>
 </html>
